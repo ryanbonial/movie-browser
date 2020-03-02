e4ae7076a2c40d7704f5885e61e09fa454c8193e
Ryan Bonial <ryanbonial@gmail.com>
2020-03-01 21:01:07 -0700

    Loads movies to db

--- servers/json-server/seed-db.js
+++ servers/json-server/seed-db.js
@@ -3,11 +3,12 @@ const fetch = require('node-fetch');
 const topMovies = require('./imdb-top-movies.json');
 const serverConfig = require('./json-server.json');
 
-
-fetch(`http://localhost:${serverConfig.port}/movies`, {
-  method: 'post',
-  body: JSON.stringify(topMovies[0]),
-  headers: { 'Content-Type': 'application/json' },
-}).then(res => res.json())
-  .then(json => console.log(json));
-
+topMovies.forEach(async movie => {
+  const res = await fetch(`http://localhost:${serverConfig.port}/movies`, {
+    method: 'post',
+    body: JSON.stringify(movie),
+    headers: { 'Content-Type': 'application/json' },
+  });
+  const json = await res.json();
+  console.log(json)
+});
