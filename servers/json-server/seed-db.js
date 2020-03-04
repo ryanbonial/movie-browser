38b12a51c8e4f25c3d1968e60457af721f1e69a2
Ryan Bonial <ryanbonial@gmail.com>
2020-03-03 21:01:48 -0700

    Slowin' things down a bit...on purpose

--- servers/json-server/seed-db.js
+++ servers/json-server/seed-db.js
@@ -3,12 +3,17 @@ const fetch = require('node-fetch');
 const topMovies = require('./imdb-top-movies.json');
 const serverConfig = require('./json-server.json');
 
-topMovies.forEach(async movie => {
-  const res = await fetch(`http://localhost:${serverConfig.port}/movies`, {
-    method: 'post',
-    body: JSON.stringify(movie),
-    headers: { 'Content-Type': 'application/json' },
-  });
-  const json = await res.json();
-  console.log(json)
-});
+async function runIt() {
+  for (let movie of topMovies) {
+    const res = await fetch(`http://localhost:${serverConfig.port}/movies`, {
+      method: 'post',
+      body: JSON.stringify(movie),
+      headers: { 'Content-Type': 'application/json' },
+    });
+    const json = await res.json();
+    console.log(json)
+    await new Promise(resolve => setTimeout(resolve, 100));
+  }
+}
+
+runIt();
